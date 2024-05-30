133243f95b222fc8e4eeee8d12976de0dfae0767
Reece Johnson <johnsonr2028@s.grafton.k12.ma.us>
2024-05-30 13:02:09 +0000

    finish later

---
+++ src/App.jsx
@@ -0,0 +1,38 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <header>
+        <h1>KOSA: Censorship is &quot;Helping&quot;</h1>
+      </header>
+
+      <h4>Table of Contents</h4>
+      <ol>
+        <a href="#what-is-kosa">
+          <li>What Is KOSA?</li>
+        </a>
+        <a href="#why-is-this-bad">
+          <li>Why Is This Bad?</li>
+        </a>
+
+        <a href="#bibliography">
+          <li>Bibliography</li>
+        </a>
+      </ol>
+      <div style={{ height: "25px" }}></div>
+      <h2 id="what-is-kosa">What Is KOSA?</h2>
+      <p>
+        KOSA (or the Kids Online Safety Act) is a bill proposed by the United
+        States Congress in Febuary 2022 and later reintroduced in May 2023. Its
+        purpose is to protect children from the dangers of the internet by
+        requiring social media sites to change their design or opt out of
+        algorithm based recomendation systems.
+        <br />
+        This bill is also the descendant of COPPA (Children&apos;s Online Privacy and Protection Act) and 
+      </p>
+    </>
+  );
+}
+
+export default App;
